26ae9062b33e16f900c8e80dbdb7993f217ac80c
Andrew Mlamba <andrew.mlamba@outlook.com>
2019-07-09 20:59:47 +0300

    updates index test file

--- src/index.test.js
+++ src/index.test.js
@@ -12,7 +12,7 @@ describe('index.html', () => {
     it('should say My First Heading', (done) => {
         const index = fs.readFileSync('./src/index.html', 'utf-8');
         jsdom.env(index, function (err, window) {
-            const h1 = window.getElementsByTagName('h1')[0];
+            const h1 = window.document.getElementsByTagName('h1')[0];
             expect(h1.innerHTML).to.equal('My First Heading');
             done();
             window.close();
