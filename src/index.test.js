090233175e20586dbee52faa5eb423d956265013
Andrew Mlamba <andrew.mlamba@outlook.com>
2019-07-19 23:02:45 +0300

    work in progress

--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('Our first test', () => {
 });
 
 describe('index.html', () => {
-    it('should say My First Heading', (done) => {
+    it('should have h1 that say Users', (done) => {
         const index = fs.readFileSync('./src/index.html', 'utf-8');
         jsdom.env(index, function (err, window) {
-            const h2 = window.document.getElementsByTagName('h2')[0];
-            expect(h2.innerHTML).to.equal('My First Heading');
+            const h1 = window.document.getElementsByTagName('h1')[0];
+            expect(h1.innerHTML).to.equal('Users');
             done();
             window.close();
         });
